e7c4af67736dc53e918564d9d2cbf6d5ed1cb40a
Esteban Garcia <garciawarneckee@gmail.com>
2018-07-15 20:31:36 -0300

    client routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const server = require("./server");
 const clientController = require("./src/controllers/client.controller");
+const clientRoutes = require("./src/routes/clients.routes");
 
-server.get("/", clientController.getByName);
+server.use("/clients", clientRoutes);
 
 server.listen(3000);
 console.log("Server up and listening in port 3000");
