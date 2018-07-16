f09eeb4e216db758e31e76c58a1b6ebcc3ef4b7c
Esteban Garcia <garciawarneckee@gmail.com>
2018-07-16 18:15:54 -0300

    fix page size calculation

--- src/controllers/policy.controller.js
+++ src/controllers/policy.controller.js
@@ -8,8 +8,8 @@ const clientService = require("../service/client.service");
  */
 exports.getByAllByName = async (req, res) => { 
   const name = (req.query.name) ? req.query.name : res.sendStatus(400);
-  const offset = (req.query.offset) ? req.query.offset : res.sendStatus(400);
-  const quantity = (req.query.quantity) ? req.query.quantity : res.sendStatus(400);
+  const offset = (req.query.offset) ? parseInt(req.query.offset) : res.sendStatus(400);
+  const quantity = (req.query.quantity) ? parseInt(req.query.quantity) : res.sendStatus(400);
 
   const client = await clientService.getByName(name);
   
