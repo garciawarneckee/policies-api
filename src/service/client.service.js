7649ab744651a748bee6b9438cb8771a4843956d
Esteban Garcia <garciawarneckee@gmail.com>
2018-07-15 21:23:37 -0300

    first policies routes

--- src/service/client.service.js
+++ src/service/client.service.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const endpoints = require("./endpoints");
+const endpoints = require("../endpoints");
 
 exports.getByName = async (name) => {
     const response = await axios.get(endpoints.clients);
